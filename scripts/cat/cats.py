89bf67cdb472d59bb7102a1825b8844a60a16a38
Coffeecats900 <136274009+Coffeecats900@users.noreply.github.com>
2025-04-28 15:26:52 -0400

    Update cats.py

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -3512,7 +3512,7 @@ def create_cat(status, moons=None, biome=None):
     elif new_cat.moons == 0:
         new_cat.moons = randint(1, 5)
 
-    not_allowed_scars = [,
+    not_allowed_scars = [
     ]
 
     for scar in new_cat.pelt.scars:
