7b68bc42dfbf9052332fb4e292ddce1df25ce52b
Coffee <Coffeecats900@gmail.com>
2025-09-24 11:16:22 -0400

    EYEFIX COBALT

--- scripts/cat/sprites.py
+++ scripts/cat/sprites.py
@@ -201,7 +201,7 @@ class Sprites:
                 "AMBER",
                 "HOLLY",
                 "IRIDESCENT",
-                "COBALT",
+                "HOTPINK",
                 "GREENYELLOW",
                 "PINKYELLOW",
                 "DAWN",
