ee701f78908b3ac0120eeb9e4b4ac84d51e4a668
Coffee <Coffeecats900@gmail.com>
2026-01-25 20:40:53 -0500

    fixed it?

--- scripts/screens/ProfileScreen.py
+++ scripts/screens/ProfileScreen.py
@@ -160,7 +160,7 @@ class ProfileScreen(Screens):
                     print("invalid next cat", self.previous_cat)
             elif event.ui_element == self.customize_cat_button:
                 self.close_current_tab()
-                self.change_screen("customize cat screen")                    
+                self.change_screen("customize cat screen")               
             elif event.ui_element == self.inspect_button:
                 self.close_current_tab()
                 self.change_screen("sprite inspect screen")
